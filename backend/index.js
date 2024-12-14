f749b3cd0977402196274bc403d85ab7fd171c21
quachkhaihao <hao.quachqkh9104cs@hcmut.edu.vn>
2024-12-14 20:51:14 +0700

    get ghe by pc cn

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,7 @@ app.use('/api/v1', require("./routes/chinhanh.Routes"))
 app.use('/api/v1', require("./routes/phongchieu.Routes"))
 app.use('/api/v1', require("./routes/pcchieucnc.Routes"))
 app.use('/api/v1', require("./routes/suatchieu.Routes"))
+app.use('/api/v1', require("./routes/ghe.Routes"))
 
 
 
